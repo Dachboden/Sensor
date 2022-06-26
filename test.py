8f8690010694bbe3da5be501bbbb40e3748e944c
Dachboden <73879434+Dachboden@users.noreply.github.com>
2022-06-26 22:00:56 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import spidev
+import time
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=1000000
+
+
+def readMCP3008(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  wert = ((adc[1]&3) << 8) + adc[2]
+  return wert
+
+if __name__ == "__main__":
+  while 1 > 0:
+    v=(readMCP3008(0)/1023.0)*3.3
+    dist =1.0 / (((v*1000) - 1125.0)/137500)
+    dist1 = 16.2537 * v**4 - 129.893 * v**3 + 382.268 * v**2 - 512.611 * v + 301.439
+    print "Distanz: %.2f cm |  v ist :%.2f "  % (dist, v)
+    time.sleep(0.01)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
